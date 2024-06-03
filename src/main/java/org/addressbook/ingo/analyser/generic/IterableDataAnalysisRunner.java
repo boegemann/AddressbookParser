2a874ad53247ebcb735b4df38cee3e090a40dd41
ingob <ingo.boegemann@gmail.com>
2024-06-03 09:10:19 +0100

    Try reading the file with CSV parser and creating an iterable

---
+++ src/main/java/org/addressbook/ingo/analyser/generic/IterableDataAnalysisRunner.java
@@ -0,0 +1,36 @@
+package org.addressbook.ingo.analyser.generic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Data Analyser implementation for Iterables
+ */
+public class IterableDataAnalysisRunner {
+
+    /**
+     * IterableDataAnalysisRunner
+     * Generic Runner Implementation to parse an iterable datasource of type T
+     * into a defined format B and analyse it with a collection of Streaming Analysers
+     * returning a Map with the result keyed by the name in the analyser map representing the analyser
+     * @param source The iterable to be analyser
+     * @param parser The parser implementation to read the row into a bean/record format
+     * @param analyserMap Mao object containing analyser implementations keyed by result names
+     * @return Map constaining the results keyed by result names
+     * @param <T> Input type
+     * @param <B> Outbut Bean type
+     */
+    public static<T,B> Map<String, String> analyse(
+            Iterable<T> source,
+            RowParser<T, B> parser,
+            Map<String, StreamingAnalyser<B>> analyserMap) {
+        for (T row : source) {
+            B parsed = parser.parseRow(row);
+            analyserMap.values().forEach(analyser ->
+                    analyser.digest(parsed)
+            );
+        }
+
+        return new HashMap<>();
+    }
+}
--- src/test/java/org/addressbook/ingo/analyser/SpecifiedScenarioRunnerTest.java
+++ src/test/java/org/addressbook/ingo/analyser/SpecifiedScenarioRunnerTest.java
@@ -1,16 +1,34 @@
 package org.addressbook.ingo.analyser;
 
+import org.addressbook.ingo.analyser.addresscsv.CsvReader;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpecifiedScenarioRunnerTest {
 
     @Test
-    public void iGivenTheProvidedAddressBookFileIGetTheSpecifiedOutputs(){
-        Map<String,String> results = new SpecifiedScenarioRunner().runSpecifiedScenario();
+    public void givenAFileResourceWithCSVRecordsICanGetAnIterable() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("AddressBook.csv"))
+        ))) {
+            Iterable<CSVRecord> csvIterable = new CsvReader().getCsvIterable(reader);
+            assertNotNull(csvIterable);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void iGivenTheProvidedAddressBookFileIGetTheSpecifiedOutputs() {
+        Map<String, String> results = new SpecifiedScenarioRunner().runSpecifiedScenario();
         assertTrue(results.containsKey("Males"));
         assertTrue(results.containsKey("Oldest Person"));
         assertTrue(results.containsKey("Age Difference Bill and Paul"));
